aebe8b445736fd0470ae891b7dcc25bbdd675130
PriyankagithubS <priyankashanmugasundaramr@gmail.com>
2024-05-26 14:09:15 +0530

    first commit

---
+++ new.js
@@ -0,0 +1,22 @@
+class Movies{
+    constructor(title,studio,rating="PG") {
+      this.title=title;
+      this.studio=studio;
+      this.rating=rating;
+    }
+    getPG(Movie){
+        return Movie.filter(Movies=>Movies.rating==="PG")
+    }
+  }
+  const myMovie =[ 
+  new Movies("Inception", "Warner Bros."),
+  new Movies("Vampier Dairies","CW Television Network","TV-14"),
+  new Movies("Bigil","AGS Entertainment"),
+  new Movies("Never have I ever","Universal studio","PG 13"),
+  new Movies("Casino Royale","Eon Productions","PG 13")
+]
+const movieI = new Movies();
+const pgMovies = movieI.getPG(myMovie); 
+console.log(pgMovies);
+//console.log(myMovie);
+  
